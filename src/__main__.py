0e997a38dfe60592f239097e5c0d4d2e5397583b
Marc-Antoine Berger <marberge@z1r6p2.42lyon.fr>
2026-04-27 20:42:24 +0200

    fixed lint error and started the basic main loop with reel response grom the llm

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,12 @@
 import sys
-from llm_sdk import Small_LLM_Model
+from llm_sdk import Small_LLM_Model  # type: ignore
 from src.core.generation_orchestrator import GenerationOrchestrator
+from src.core.prompt_builder import PromptBuilder
 from src.utils.parser import get_args
 
 
 def main() -> None:
+    # ==========================  PARSING  =============================
     try:
         functions, prompts = get_args()
 
@@ -14,18 +16,23 @@ def main() -> None:
 
     except Exception as e:
         print("[ERROR] An error occured during "
-              f"parsing :\n {e}")
+              f"parsing :\n {e}\n")
         sys.exit(1)
 
-    # ==================================================================
+    # =========================  GENERATION  =============================
 
     try:
         llm = Small_LLM_Model()
-        orchestrator = GenerationOrchestrator(llm)
-        orchestrator.run_generation(functions, prompts)
+
+        prompter = PromptBuilder(functions)
+
+        orchestrator = GenerationOrchestrator(llm, prompter)
+        orchestrator.run_generation(prompts)
     except Exception as e:
         print("[ERROR] An error occured during initialization or "
-              f"running the main loop of generation :\n {e}")
+              f"running the main loop of generation :\n {e}\n")
+
+    # ====================================================================
 
 
 if __name__ == "__main__":
